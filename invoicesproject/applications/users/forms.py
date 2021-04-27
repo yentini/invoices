bb561d73962301272377d6658975a376c927680d
César Cabrerizo de Gregorio <ccabrerizo@indra.es>
2021-04-27 09:00:21 +0200

    Initial configuration

---
+++ invoicesproject/applications/users/forms.py
@@ -0,0 +1,122 @@
+from datetime import *
+
+from django import forms
+from django.contrib.auth import authenticate
+#
+from .models import User
+
+class UserRegisterForm(forms.ModelForm):
+
+    password1 = forms.CharField(
+        label='Contraseña',
+        required=True,
+        widget=forms.PasswordInput(
+            attrs={
+                'placeholder': 'Contraseña'
+            }
+        )
+    )
+    password2 = forms.CharField(
+        label='Contraseña',
+        required=True,
+        widget=forms.PasswordInput(
+            attrs={
+                'placeholder': 'Repetir Contraseña'
+            }
+        )
+    )
+
+    class Meta:
+        """Meta definition for Userform."""
+
+        date_today = date.today()
+        date_format = (date_today - timedelta(days=date_today.day)).strftime("%Y-%m-%d")
+
+        model = User
+        fields = (
+            'email',
+            'full_name',
+            'ocupation',
+            'genero',
+            'date_birth',
+        )
+        widgets = {
+            'email': forms.EmailInput(
+                attrs={
+                    'placeholder': 'Correo Electronico ...',
+                }
+            ),
+            'full_name': forms.TextInput(
+                attrs={
+                    'placeholder': 'nombres ...',
+                }
+            ),
+            'ocupation': forms.TextInput(
+                attrs={
+                    'placeholder': 'Ocupacion ...',
+                }
+            ),
+            'date_birth': forms.DateInput(
+                attrs={
+                    'type': 'date', 
+                    'value': date_format,
+                },
+            ),
+        }
+    
+    def clean_password2(self):
+        if self.cleaned_data['password1'] != self.cleaned_data['password2']:
+            self.add_error('password2', 'Las contraseñas no son iguales')
+
+
+class LoginForm(forms.Form):
+    email = forms.CharField(
+        label='E-mail',
+        required=True,
+        widget=forms.TextInput(
+            attrs={
+                'placeholder': 'Correo Electronico',
+            }
+        )
+    )
+    password = forms.CharField(
+        label='Contraseña',
+        required=True,
+        widget=forms.PasswordInput(
+            attrs={
+                'placeholder': 'contraseña'
+            }
+        )
+    )
+
+    def clean(self):
+        cleaned_data = super(LoginForm, self).clean()
+        email = self.cleaned_data['email']
+        password = self.cleaned_data['password']
+
+        if not authenticate(email=email, password=password):
+            raise forms.ValidationError('Los datos de usuario no son correctos')
+        
+        return self.cleaned_data
+
+
+class UpdatePasswordForm(forms.Form):
+
+    password1 = forms.CharField(
+        label='Contraseña',
+        required=True,
+        widget=forms.PasswordInput(
+            attrs={
+                'placeholder': 'Contraseña Actual'
+            }
+        )
+    )
+    password2 = forms.CharField(
+        label='Contraseña',
+        required=True,
+        widget=forms.PasswordInput(
+            attrs={
+                'placeholder': 'Contraseña Nueva'
+            }
+        )
+    )
